ea2c1b14955d98efc7c000b3df3057f4fd34b8ad
DELETED ACCOUNT 100% <66882633+darkdarcool@users.noreply.github.com>
2021-03-19 11:38:31 -0400

    Update greet.ts

--- src/bot/commands/greet.ts
+++ src/bot/commands/greet.ts
@@ -1,1 +1,18 @@
+import Command from '../struct/Command';
+import { Message } from 'discord.js';
 
+abstract class HelloCommand extends Command {
+  constructor() {
+    super({
+      name: 'hello',
+      aliases: ['p'],
+      description: 'Will return hello back to the user',
+    });
+  }
+
+  exec(message: Message) {
+    return message.reply('Hello! I am DarkBot, a discord bot that you can talk to and play games with!');
+  }
+}
+
+export default HelloCommand;
