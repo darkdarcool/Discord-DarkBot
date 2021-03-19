1f688df7b4f46c8d4b29cce093a6b3b965a6cd6a
DELETED ACCOUNT 100% <66882633+darkdarcool@users.noreply.github.com>
2021-03-19 11:27:26 -0400

    Update settings.example.ts

--- src/bot/settings.example.ts
+++ src/bot/settings.example.ts
@@ -1,8 +1,8 @@
 import { Settings } from './types/Settings';
 
 const settings: Settings = {
-  BOT_TOKEN: 'BOT TOKEN',
-  BOT_OWNER_ID: ['OWNER ID'],
+  BOT_TOKEN: 'C2cn4ur_oM8lvIJUo9ZmuWYg1m6wfDtv',
+  BOT_OWNER_ID: ['C2cn4ur_oM8lvIJUo9ZmuWYg1m6wfDtv'],
   PREFIX: '!'
 }
 
